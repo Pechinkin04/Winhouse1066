fc9fa9f99d5e4b655bee4fd670c3a3098aef1b9d
Alexander Pechinkin <pechinkin04@yandex.ru>
2024-11-24 19:41:09 +0400

    Initial Commit

---
+++ Winhouse1066/Winhouse1066App.swift
@@ -0,0 +1,17 @@
+//
+//  Winhouse1066App.swift
+//  Winhouse1066
+//
+//  Created by Александр Печинкин on 24.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Winhouse1066App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
